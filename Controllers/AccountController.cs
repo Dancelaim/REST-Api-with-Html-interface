b39bb026cba36b94b4e76b18cd9b855025d5498e
dancelaim <dancelaim@gmail.com>
2021-05-18 20:56:48 +0500

    Добавьте файлы проекта.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebApI.Model;
+using WebApI.Security;
+
+namespace WebApI.Controllers
+{
+    public class AccountController : Controller
+    {
+        Context db;
+        public AccountController(Context context)
+        {
+            db = context;
+        }
+
+        [HttpPost("/token")]
+        public IActionResult Token()
+        {
+
+                string authHeader = HttpContext.Request.Headers["Authorization"];
+                if (authHeader != null)
+                {
+                    var authHeaderValue = AuthenticationHeaderValue.Parse(authHeader);
+                    if (authHeaderValue.Scheme.Equals(AuthenticationSchemes.Basic.ToString(), StringComparison.OrdinalIgnoreCase))
+                    {
+                        var credentials = Encoding.UTF8
+                                            .GetString(Convert.FromBase64String(authHeaderValue.Parameter ?? string.Empty))
+                                            .Split(':', 2);
+                        if (credentials.Length == 2)
+                        {
+                            var identity = GetIdentity(credentials[0], credentials[1]);
+                            if (identity == null)
+                            {
+                                return Unauthorized(new { errorText = "Invalid username or password." });
+                            }
+                            var now = DateTime.UtcNow;
+                            var jwt = new JwtSecurityToken(issuer: AuthOptions.ISSUER, audience: AuthOptions.AUDIENCE, notBefore: now, claims: identity.Claims,
+                                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+                            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+                            var response = new
+                            {
+                                access_token = encodedJwt,
+                                username = identity.Name
+                            };
+                            return Json(response);
+                        }
+                    }
+                }
+            return Unauthorized();
+
+
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User User = db.Users.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (User != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, User.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, User.Role)
+                };
+                ClaimsIdentity claimsIdentity =  new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebApI.Model;
+using WebApI.Security;
+
+namespace WebApI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdb1;Trusted_Connection=True;";
+            services.AddDbContext<Context>(options => options.UseSqlServer(con));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = AuthOptions.ISSUER,
+                            ValidateAudience = true,
+                            ValidAudience = AuthOptions.AUDIENCE,
+                            ValidateLifetime = true,
+
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
